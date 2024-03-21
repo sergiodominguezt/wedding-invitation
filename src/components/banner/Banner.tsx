f3a06e306475426b400c690606fdd9960dca51c3
sergdt <sergiodominguez9710@gmail.com>
2024-03-21 14:34:40 -0500

    fixed banner component loading time

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,16 +1,37 @@
+import { useState, useEffect } from "react";
 import banner from "../../assets/banner.jpg";
 import "./banner.css";
 
 const Banner = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = banner;
+    img.onload = () => setImageLoaded(true);
+  }, []);
+
   return (
-    <div className="banner">
-      <img src={banner} alt="Imagen de boda" className="responsive-image" />
-      <div className="texto">
-        <h1>¡Nos casamos!</h1>
-      </div>
-      <div className="banner-date">
-        <h2>29 . 06 . 2024</h2>
-      </div>
+    <div
+      className="banner"
+      style={{ visibility: imageLoaded ? "visible" : "hidden" }}
+    >
+      <img
+        src={banner}
+        alt="Imagen de boda"
+        className="responsive-image"
+        onLoad={() => setImageLoaded(true)}
+      />
+      {imageLoaded && (
+        <>
+          <div className="texto">
+            <h1>¡Nos casamos!</h1>
+          </div>
+          <div className="banner-date">
+            <h2>29 . 06 . 2024</h2>
+          </div>
+        </>
+      )}
     </div>
   );
 };
